ee09a8cb3a5806e05648b2eee0b7a41c42cf4752
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-06-12 15:09:41 +0300

    add install script

---
+++ src/install.sh
@@ -0,0 +1,11 @@
+# Upgrade pip
+python3 -m pip install --upgrade pip
+
+# Install PyTorch and torchvision
+python3 -m pip install torch torchvision
+
+# Install TensorFlow
+python3 -m pip install tensorflow
+
+# Install the MNIST dataset handler
+python3 -m pip install mnist

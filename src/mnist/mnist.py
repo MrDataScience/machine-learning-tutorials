720f150cd6a88cc13c2e405ff024f8d7ce4f1a93
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-06-13 13:58:24 +0300

    uses custom lambda to shift pix values

--- src/mnist/mnist.py
+++ src/mnist/mnist.py
@@ -1,36 +1,44 @@
 import torch
 from torch.utils.data import Dataset
 from torchvision import datasets
-from torchvision.transforms import ToTensor
+from torchvision.transforms import Compose, Lambda, ToTensor
 from torchvision.transforms import v2
-from torchvision.transforms import Compose, Lambda
 import matplotlib.pyplot as plt
 
 
 # source: https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
 
-def erase_middle(image: torch.Tensor) -> torch.Tensor:
-    all, height, width = image.size()
-    image[:, 10:20, :] = image[:, 14, :]
+def change_pix_values(image: torch.Tensor, row_i: int, row_j: int, pix_val: float) -> torch.Tensor:
+    """
+    lambda that will change all pixels from `row_i:row_j` to a new value, given by `pix_val`
+    """
+    _, height, _ = image.size()
 
+    # image[:, row_i:row_j, :] = image[:, int(height/2), :]
+    image[:, row_i:row_j, :] = pix_val
     return image
 
 
+class MyLambda(Lambda):
+    """custom lambda class that will support arguments"""
+
+    def __init__(self, lambd, row_i, row_j, pix_val):
+        super().__init__(lambd)
+        self.row_i = row_i
+        self.row_j = row_j
+        self.pix_val = pix_val
+
+    def __call__(self, img, ):
+        return self.lambd(img, self.row_i, self.row_j, self.pix_val)
+
+
 def load_data(data_set, transformer) -> tuple:
-    special = Compose(
-        [
-            # First transform it to a tensor
-            ToTensor(),
-            # Then erase the middle
-            Lambda(erase_middle),
-        ]
-    )
 
     training_data = data_set(
         root="data",
         train=True,
         download=True,
-        transform=special
+        transform=transformer
     )
 
     test_data = data_set(
@@ -55,10 +63,31 @@ def plot_data(data, fig_size, cols, rows):
     plt.show()
 
 
+def get_transformers():
+    """
+    return two transformers, second one supports v2
+    """
+    t1 = Compose([
+        # First transform it to a tensor
+        ToTensor(),
+        # Then erase the middle
+        MyLambda(change_pix_values, 10, 20, 0.5),
+    ])
+
+    t2 = v2.Compose([
+        v2.ToImage(),
+        v2.ToDtype(torch.float32, scale=True),
+        MyLambda(change_pix_values, 10, 20, 0.5),
+    ])
+
+    return t1, t2
+
+
 if __name__ == "__main__":
-    transformer = ToTensor()
+    transformer, transformer_v2 = get_transformers()
+
     training_data, test_data = load_data(
-        datasets.MNIST, transformer=transformer)
+        datasets.MNIST, transformer=transformer_v2)
 
     # Constants
     COLS, ROWS = 3, 3
